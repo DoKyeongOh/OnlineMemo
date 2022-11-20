7a998567242846419cf797fdd399f131d1e10b48
DoKyeong Oh <dkproh@gmail.com>
2022-11-20 23:52:32 +0900

    category 개체를 생성, 조회하는 핸들러 메소드 작성

--- src/main/java/kr/couchcoding/onlinememodemo/controller/CategoryController.java
+++ src/main/java/kr/couchcoding/onlinememodemo/controller/CategoryController.java
@@ -1,15 +1,33 @@
 package kr.couchcoding.onlinememodemo.controller;
 
 import kr.couchcoding.onlinememodemo.dto.CategoryDTO;
+import kr.couchcoding.onlinememodemo.entity.Category;
+import kr.couchcoding.onlinememodemo.service.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/categories")
+@AllArgsConstructor
 public class CategoryController {
 
+    private final CategoryService categoryService;
+
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable Long id){
+        return categoryService.getCategoryById(id);
+    }
+
+    @GetMapping("")
+    public Page<Category> getCategories(Pageable pageable, @RequestParam String keyword){
+        return categoryService.getCategories(pageable, keyword);
+    }
+
     @PostMapping("")
-    public CategoryDTO categoryDTO(@RequestBody CategoryDTO categoryDTO){
-        return categoryDTO;
+    public Category categoryDTO(@RequestBody CategoryDTO categoryDTO){
+        return categoryService.createCategory(categoryDTO);
     }
 
 }
